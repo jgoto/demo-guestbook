fea73c46579019e63395e25fda6457cb2f261aba
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2025-02-26 23:08:32 -0500

    Got basic app working with the connection with the database

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
-import { useState } from 'react'
-
-
 import './App.css'
-
-
-console.log("SUPABASE URL:", import.meta.env.VITE_SUPABASE_URL);
-console.log("SUPABASE API KEY: ", import.meta.env.VITE_SUPABASE_API_KEY);
+import { useEffect, useState } from 'react';
+import supabase from './supabaseClient';
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  useEffect(()=>{
+    const fetchMessage = async ()=> {
+      const {data, error} = await supabase.from("messages").select("*").order("created_at",{ascending: false});
+      if(error)
+        console.error(error);
+      else setMessages(data)
+    };
+    fetchMessage();
+  },[]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!newMessage.trim()) return;
+
+    const {data, error} = await supabase.from("messages").insert([{content: newMessage}]);
+    if(error)
+      console.error(error);
+    else setMessages([data[0], ...messages]);
+    setNewMessage("");
+  }
 
   return (
     <div>
       <h1>Demo Guestbook</h1>
+      <form onSubmit={handleSubmit}>
+        <input type = "text" value={newMessage} onChange={(e)=>setNewMessage(e.target.value)} placeholder="Write a message..."/>
+        <button type="submit">Send</button>
+      </form>
+      <ul>
+        {messages.map((msg)=>(
+          <li key={msg.id}>{msg.content}</li>
+        ))}
+      </ul>
     </div>
   )
 }
