757ac714093550e4561a1dba1f658776d58448ad
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2025-11-12 09:44:47 -0500

    Added backend server with ping function to test api

---
+++ service-api/server.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/ping', (req, res) => {
+    return res.json({message: "Pong"});
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+});
