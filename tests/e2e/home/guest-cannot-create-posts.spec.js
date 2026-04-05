79702650a3a6e41f790cbad0cd7c16b67677cd09
Jin Nan Goto <43223423+jgoto@users.noreply.github.com>
2026-04-04 20:26:21 -0400

    Added ability for playwright to save user authentication into storageStates for saving authentication states in between tests

--- tests/e2e/home/guest-cannot-create-posts.spec.js
+++ tests/e2e/home/guest-cannot-create-posts.spec.js
@@ -1,6 +1,8 @@
 import {test, expect} from '@playwright/test';
 import HomePage from '../../pom/pages/HomePage';
 
+test.use({storageState: {cookies: [], origins: []}});
+
 test('Guest to be restricted from making posts by hiding option', async ({page}) => {
     const homePage = new HomePage(page);
 
--- tests/e2e/login/login.spec.js
+++ tests/e2e/login/login.spec.js
@@ -1,11 +1,13 @@
 import {test, expect} from '@playwright/test';
 import LoginPage from '../../pom/pages/LoginPage';
 
+test.use({storageState: {cookies: [], origins: []}});
+
 test('user filling in email and password and clicking "login" succesfully logs them in', async ({page}) => {
     const loginPage = new LoginPage(page)
 
     await loginPage.goto();
-    await loginPage.login('test@example.com', 'REMOVED');
+    await loginPage.login(process.env.TEST_USER_EMAIL, process.env.TEST_USER_PW);
 
     await expect(page).toHaveURL(/\/$/);  // This will do for now, but we need a more robust means of verifying test
 })
